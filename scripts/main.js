3dde1503065d2f9b0e2bfc514a001e22a8b53793
Ytalo Duenas <ytaloduenas@gmail.com>
2021-05-22 13:50:04 -0500

    My First Commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+/*
+function myFunction() {
+	var x = document.getElementById("MyMenu-Items");
+	if (x.className === "Menu-Items") {
+		x.className += " responsive";
+	} else {
+		x.className = "Menu-Items";
+	}
+}
+*/
+
+function myFunction() {
+	var x = document.getElementById("myMenu-Items");
+	if (x.className === "Menu-Items") {
+		x.className += " responsive";
+	} else {
+		x.className = "Menu-Items";
+	}
+}
